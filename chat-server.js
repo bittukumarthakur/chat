42a562cfa5ddc3f468910885c96af7380d330613
Bittu <bittukumarthakur@users.noreply.github.com>
2023-07-28 18:09:35 +0530

    Add: clients, refactor on data event of socket in chatServer

--- chat-server.js
+++ chat-server.js
@@ -2,14 +2,25 @@ const net = require("node:net");
 
 const createChatServer = () => {
   const chatServer = net.createServer();
-
+  const clients = {};
   chatServer.listen(8000);
+
   chatServer.on("connection", (socket) => {
     socket.setEncoding("utf-8");
+
     console.log("Connection Established");
 
     socket.on("data", (data) => {
-      console.log(JSON.parse(data));
+      const messageChunk = JSON.parse(data);
+      const { sender, receiver, message } = messageChunk;
+
+      if (!(sender in clients)) {
+        clients[sender] = socket;
+      }
+
+      if (message) {
+        clients[receiver].write(`${sender}<< ${message}`);
+      }
     });
   });
 };
